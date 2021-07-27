fbbb0e97cc77d86fbed54fa9e9137e9930f36c0b
RamonJales <ramonjales123@gmail.com>
2021-07-27 16:36:34 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+- GAME OBJECTIVE:
+
+1. Nim is a strategy game in which two players take turns removing pieces. On each turn, a player must remove at least one piece up to the maximum number of removals established at the beginning of the game. Whoever gets the last object wins.
+2. In this case, the program is being programmed to always win. It does not matter what you do.
+3. I took this lesson from the usp computer science course at coursera.
+
+- MY OBJECTIVES:
+
+1. I intend to apply the things what i learn over time. This code starts simple and i will improve it little by little.
+2. I just want to improve my coding skills and anyone is willing to give me tips and suggestionos to improve my code, i will be grateful.
+
+- FIRST COMMIT:
+1. This first code is composed of functions in a single class. I will implement the classes later.
---
+++ src/application/MainProgram.java
@@ -0,0 +1,139 @@
+package application;
+
+import java.util.Scanner;
+
+public class MainProgram {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Welcome to the NIM game! Choice: ");
+		System.out.println();
+		System.out.println("1 - To play a single game");
+		System.out.println("2 - To play a championship");
+		System.out.print("Option: ");
+		int option = sc.nextInt();
+		
+		if (option == 2) {
+			System.out.println();
+			System.out.println("You chose a championship!");
+			championship();
+		}
+		else {
+			System.out.println();
+			match();
+		}
+		sc.close();
+	}
+	
+	public static int userChoosesMove(int totalPieces, int piecesLimit) {
+		Scanner sc = new Scanner(System.in);
+		
+		int userMove;
+		
+		while (true) {
+			System.out.print("How many pieces are you going to take out? ");
+			userMove = sc.nextInt();
+			
+			if (userMove > piecesLimit || userMove < 1) {
+				System.out.println();
+				System.out.println("Ops! Invalid move, try again!");
+				System.out.println();
+			}
+			else {
+				break;
+			}
+		}
+		sc.close();
+		
+		return userMove;
+	}
+	
+	public static int computerChoosesMove(int totalPieces, int piecesLimit) {
+		
+		int computerMove = 1;
+		
+		while (computerMove != piecesLimit) {
+			if ((totalPieces - computerMove) % (piecesLimit + 1) == 0) {
+				return computerMove;
+			}
+			else {
+				computerMove++;
+			}
+		}
+		return computerMove;
+	}
+	
+	public static void match() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Starting pieces of the game: ");
+		int totalPieces = sc.nextInt();
+		System.out.print("Limit of pieces per move: ");
+		int piecesLimit = sc.nextInt();
+		
+		boolean computerTurn = false;
+		
+		if (totalPieces % (piecesLimit + 1) == 0) {
+			System.out.println();
+			System.out.println("You start!");
+			System.out.println();
+		}
+		else {
+			System.out.println();
+			System.out.println();
+			System.out.println("Computer start!");
+			computerTurn = true;
+		}
+		
+		while (totalPieces > 0) {
+			if (computerTurn) {
+				int removedPiecesComputer = computerChoosesMove(totalPieces, piecesLimit);
+				totalPieces -= removedPiecesComputer;
+				if (removedPiecesComputer == 1) {
+					System.out.println();
+					System.out.println("The computer removed one piece!");
+				}
+				else {
+					System.out.println();
+					System.out.printf("The computer removed %d piece!\n", removedPiecesComputer);
+				}
+				computerTurn = false;
+			}
+			else {
+				int removedPiecesUser = userChoosesMove(totalPieces, piecesLimit);
+				totalPieces -= removedPiecesUser;
+				if (removedPiecesUser == 1) {
+					System.out.println();
+					System.out.println("The computer removed one piece!");
+				}
+				else {
+					System.out.println();
+					System.out.printf("The computer %d pieces!\n", removedPiecesUser);
+				}
+				computerTurn = true;
+			}
+			if (totalPieces == 1) {
+				System.out.println("Now only one piece remains on the board.");
+			}
+			else if (totalPieces != 0){
+				System.out.printf("Now only %d piece remains on the board.\n", totalPieces);
+				System.out.println();
+			}
+		}
+		System.out.println("End game! Computer won!");
+		sc.close();
+	}
+	
+	public static void championship() {
+		for (int i = 1; i <= 4; i++) {
+			System.out.println();
+			System.out.printf("**** Round %d ****\n", i);
+			System.out.println();
+			match();
+		}
+		System.out.println();
+		System.out.println("Score: You 0 X 3Computer");
+	}
+}
